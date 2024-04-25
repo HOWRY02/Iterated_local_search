d67fde98d0fd54204d2487b5f8dae0e2dbc52d36
Phuc <huynhvinhphuc101@gmail.com>
2024-04-25 21:16:11 +0700

    read data csv

--- src/construction_heuristic.py
+++ src/construction_heuristic.py
@@ -1,10 +1,39 @@
-# import 
-
+import pandas as pd
 
 class ConstructionHeuristic:
-    pass
+    def __init__(self):
+        # Capacity of tank vessel (kg)
+        self.Q = 30000
+
+    def compute_inventory_level(self, C, T):
+        """
+        Convert a sheet of sample writing input to a custom directory structure of PNGs.
+
+        Parameters
+        ----------
+        C : list
+            Set of customers C = {1,..., N}.
+        T : list
+            Set of time periods T = {1,...,T}.
+        """
+        for customer in C:
+            for time_period in T:
+                inventory =  initial_inventory[customer] - 
+        
+    def test():
+        pass
+
+    def run(self, ):
+
+        S_star = []
+        ratio_demand = 1.0
+
+        while ratio_demand > 0:
+            inventory_level = self.compute_inventory_level()
+
 
 
 
 if __name__ == "__main__":
-    pass
+    df = pd.read_csv('data/order_data.csv')
+    
